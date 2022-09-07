abb4070096794acce09d90b963fb6ce6d394a79b
smithaalizina <smitha.aries88@gmail.com>
2022-09-07 19:43:03 +0530

    Alert Test

---
+++ src/main/testNG/AlertTest.java
@@ -0,0 +1,25 @@
+package testsuite.testNG;
+
+import Driver.BaseClass;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import pages.CalendarPage;
+
+public class AlertTest extends BaseClass {
+
+    @Test(priority = 1)
+    public void handleAlerts(){
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        //CalendarPage calendarPage = new CalendarPage();
+        //calendarPage.isAlertPresent();
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+        wait.until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert();
+        alert.dismiss();
+    }
+}
---
+++ src/main/testNG/DropdownTest.java
@@ -0,0 +1,45 @@
+package testsuite.testNG;
+
+import Driver.BaseClass;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import pages.CategoriesPage;
+import pages.DashboardPage;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class DropdownTest extends BaseClass {
+
+    @Test(priority = 2)
+    public void selectFromDropdown() {
+
+        DashboardPage dashboardPage = new DashboardPage();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, 60);
+           // dashboardPage.categories.isEnabled();
+            wait.until(ExpectedConditions.visibilityOf(dashboardPage.stratergy)).click();
+
+        }
+        catch (ElementNotVisibleException e)
+        {
+            System.out.println("element not found"+e);
+        }
+
+
+        CategoriesPage categoriesPage=new CategoriesPage();
+       // categoriesPage.categories.isDisplayed();
+        categoriesPage.categories.click();
+        Select select= new Select(categoriesPage.categories);
+        select.selectByVisibleText("Issues");
+
+    }
+    }
+
